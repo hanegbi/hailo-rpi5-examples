dc5f04ce278c54902f1accbd18929b7150949cd5
Dan Hanegbi <danh@hailo.ai>
2024-12-25 23:02:05 +0200

    Added stop to gui

--- community_projects/sailted_fish/start_gui.py
+++ community_projects/sailted_fish/start_gui.py
@@ -1,8 +1,10 @@
 import gi
-gi.require_version('Gtk', '3.0')
-from gi.repository import Gtk
 import subprocess
 import os
+import signal
+
+gi.require_version('Gtk', '3.0')
+from gi.repository import Gtk
 
 class MainGUI(Gtk.Window):
     def __init__(self):
@@ -22,6 +24,11 @@ class MainGUI(Gtk.Window):
         self.start_button.connect("clicked", self.on_start_button_clicked)
         self.box.pack_start(self.start_button, False, False, 10)
 
+        # Add the Stop button
+        self.stop_button = Gtk.Button(label="Stop")
+        self.stop_button.connect("clicked", self.on_stop_button_clicked)
+        self.box.pack_start(self.stop_button, False, False, 10)
+
     def on_start_button_clicked(self, widget):
         """Callback for the Start button."""
         try:
@@ -29,6 +36,20 @@ class MainGUI(Gtk.Window):
             script_path = os.path.join(os.path.dirname(__file__), "sailted_fish.py")
             subprocess.Popen(["python3", script_path, "--input", "rpi"])
             self.label.set_text("Game started!")
+        except Exception as e:
+            self.label.set_text(f"Error: {e}")
+
+    def on_stop_button_clicked(self, widget):
+        """Callback for the Stop button."""
+        try:
+            # Find and terminate processes containing "Hailo Pose"
+            result = subprocess.run(["pgrep", "-f", "Hailo Pose"], stdout=subprocess.PIPE, text=True)
+            if result.stdout:
+                for pid in result.stdout.split():
+                    os.kill(int(pid), signal.SIGTERM)
+                self.label.set_text("Game stopped!")
+            else:
+                self.label.set_text("No Hailo Pose process found.")
         except Exception as e:
             self.label.set_text(f"Error: {e}")
 
