20f7232ca05179fa8ccb53040067b94ca81a8420
Dan Hanegbi <danh@hailo.ai>
2024-12-25 21:35:03 +0200

    Fix red

--- basic_pipelines/sailted_fish.py
+++ basic_pipelines/sailted_fish.py
@@ -65,7 +65,6 @@ def game_loop():
         # time.sleep(5)  # Duration for Green Light
 
         # Red Light phase
-        game_state = "Red Light"
         print("\033[30;45m!!! 1 !!!\033[0m")
         time.sleep(1)
         print("\033[30;45m!!! 2 !!!\033[0m")
@@ -74,6 +73,7 @@ def game_loop():
         time.sleep(1)
         print("\033[30;45mSailted Fish\033[0m")
         print("\033[30;45mSTOPPPPPPP\033[0m")
+        game_state = "Red Light"
         time.sleep(20)  # Duration for Red Light
 
         # Determine winner during Red Light
